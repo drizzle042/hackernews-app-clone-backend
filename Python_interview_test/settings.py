e9b0607a3cbf82111bc6aee1eeadc05a449fed35
Chukwujiobi <chukwujiobicanon@gmail.com>
2022-09-23 17:57:09 +0100

    Production version 1.2

--- Python_interview_test/settings.py
+++ Python_interview_test/settings.py
@@ -83,8 +83,12 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hackernews_db',
+        'USER': 'drizzle',
+        'PASSWORD': 'Keshadel1',
+        'HOST': 'db4free.net',
+        'PORT': '3306'
     }
 }
 
